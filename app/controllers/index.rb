6616015e84c1154e763fe3f20042c3f50e21f381
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 18:41:12 -0500

    added forms

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,30 @@
+# GET ===========================
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/user' do
+  # Look in app/views/index.erb
+  erb :user
+end
+
+get '/user/new' do
+  # Look in app/views/index.erb
+
+  erb :new_user
+end
+
+get '/user/login' do
+  # Look in app/views/index.erb
+  erb :login
+end
+
+# POST ===========================
+
+post '/user' do
+  new_user = User.create(params[:user])
+  erb :user
+end
+
