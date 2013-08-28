f24e04183f305a27c529f47e3e26940d7b47553c
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 18:42:37 -0500

    added views

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Remember to create a migration!
+end
+
+# start with empty DB
+# populate w/ users using the create method through the view
+# index contains sign up or log in links
+  # sign up and log in links go to relative views 
+  # SIGN UP :
+    # field for name
+    # field for email
+    # field for password
+  # LOG IN :
+    # field for email
+    # field for password 
+
+
